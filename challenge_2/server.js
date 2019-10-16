faa82ca8815586fc0f143f9c6bfba972808c1b41
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-16 17:41:38 -0400

    Adds server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res, next) => {
+  // response goes here
+});
 
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+})
