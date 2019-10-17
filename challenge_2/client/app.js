1ba2646c19d8c3a97445db13aa8bf40d6955e878
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-17 16:54:04 -0400

    Adds AJAX request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,46 @@
+$(() => {
+  const $btn = $('button');
 
+  $btn.on('click', (e) => {
+    // debugger;
+    console.log('clicked');
+    handleClick();
+  })
+
+  // set up a jQuery ajax call to trigger on button click
+  // does this need to be enctype=multipart/form-data
+
+  function handleClick() {
+
+    // get a reference to the File the user uploaded
+    const file = $('#jsonFile').get(0).files[0];
+
+    // debugger;
+    // TODO: is it better to process the file into JSON on the client, or send the file to the server as binary data?
+
+    file.text()
+      .then((fileAsJSONString) => {
+        makeAjaxRequest(fileAsJSONString);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  function makeAjaxRequest(file) {
+    $.ajax({
+      url: `http://127.0.0.1:3000/json`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: file,
+      processData: false,
+      success: (data, status, jqXHR) => {
+        console.log(data);
+      },
+      error: (jqXHR, textStatus, error) => {
+        console.log(error);
+      }
+    });
+  }
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const utilities = require('./utilities.js');
 const multer = require('multer');
 const upload = multer();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('client'));
 
@@ -14,6 +19,8 @@ app.get('/', (req, res, next) => {
 });
 
 app.post('/', upload.single('jsonFile'), (req, res, next) => {
+  console.log('req.body: ', req.body);
+
   const json = req.body.jsonText === '' ? req.file.buffer.toString() : req.body.jsonText;
   const csv = utilities.JSONtoCSV(json);
 
@@ -43,6 +50,15 @@ app.post('/', upload.single('jsonFile'), (req, res, next) => {
   res.write(html);
 
 });
+
+app.post('/json', (req, res, next) => {
+  console.log('req: ', req);
+  console.log('req.body: ', req.body);
+
+  const csv = utilities.JSONtoCSV(JSON.stringify(req.body));
+  res.status(200);
+  res.send(csv);
+})
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
