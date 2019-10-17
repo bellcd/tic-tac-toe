44875a547616038739497e8672fa854d312f05c8
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-16 20:42:12 -0400

    Adds urlencoded parsing middleware

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('client'));
+
 app.get('/', (req, res, next) => {
-  // response goes here
+  console.log('here');
+});
+
+app.post('/', (req, res, next) => {
+  console.log('req.body.json_data: ', req.body.json_data);
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
