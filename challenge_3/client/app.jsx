873379d62e2e915c9cf857f6039d8ff3bd5899bb
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-22 01:28:34 -0400

    [wip] Adds basic React components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,8 +2,49 @@
 // const React = require('react');
 // const ReactDOM = require('reactDOM');
 
-const App = () => {
-  return 'hi there!';
+const Checkout = ({ onClick }) => {
+  return (
+    <button
+      id="checkout-button"
+      onClick={onClick}
+    >
+    Checkout
+    </button>
+  );
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      address_line_1: '',
+      address_line_2: '',
+      city: '',
+      state: '',
+      zip_code: '',
+      phone_num: '',
+      cc_number: '',
+      cc_exp: '',
+      cvv: '',
+      zip_code_billing: ''
+    };
+
+    this.HandleCheckoutClick = this.handleCheckoutClick.bind(this);
+  }
+
+  handleCheckoutClick(e) {
+    console.log(e.target);
+  }
+
+  render() {
+    return <div>
+      <Checkout onClick={this.handleCheckoutClick}></Checkout>
+    </div>
+  }
 };
 
 ReactDOM.render(<App></App>, document.querySelector('#root'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 const connection = require('./db.js').connection;
 
 app.listen(port, () => { console.log(`App is listening on port ${port}`) });
