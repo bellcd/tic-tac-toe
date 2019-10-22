ef90583dab9bf0deb33ccf038a3210a236dc9a3e
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-21 21:52:53 -0400

    [wip] Sets up build environment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
+// TODO: why did I think I needed require() statements to use React??
+// const React = require('react');
+// const ReactDOM = require('reactDOM');
+
+const App = () => {
+  return 'hi there!';
+};
+
+ReactDOM.render(<App></App>, document.querySelector('#root'));
 
 
