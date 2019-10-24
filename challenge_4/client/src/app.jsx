802975dde62cf29a62fb716a0701740bf8b3131c
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-24 01:15:51 -0400

    Makes discs fall properly

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -8,6 +8,7 @@ class App extends React.Component {
     this.handleClick = this.handleClick.bind(this);
 
     this.state = {
+      count: 0, // TODO: better way to keep track of count??
       // each subarray is a row
       // 0 is red, 1 is yellow
       gridRep: [ // there's probably a better way to set every as null to begin with ...
@@ -30,9 +31,43 @@ class App extends React.Component {
   }
 
   handleClick(e, x, y) {
-    debugger;
-    console.log(e);
+    this.setDisc(x, y);
+  }
 
+  // TODO: there's probably a better way to do this ...
+  makeDiscFall(x, y, state) {
+    // check the relevant x index of all the other rows (y's)
+    const col = state.gridRep.map(row => {
+      return row[x];
+    })
+    // get the highest index disc that has a value of null (0,0 is at the TOP LEFT of the grid)
+    // loop through the col backwards (ie, from the bottom of the grid up)
+    for (let i = col.length - 1; i > -1; i--) {
+      if (col[i] === null) {
+        // return the index of the first null value
+        return i;
+      }
+    }
+  }
+
+  // TODO: need to check if count is at max
+
+  setDisc(x, y) {
+    this.setState((state, props) => {
+      // determine whose turn it is
+      let disc = state.count % 2 === 0 ? 0 : 1;
+
+      // apply makeDiscFall, and reassign y
+      y = this.makeDiscFall(x, y, state);
+
+      // update the grid
+      state.gridRep[y][x] = disc;
+
+      return {
+        gridRep: state.gridRep,
+        count: ++state.count
+      }
+    });
   }
 
   render() {
