1dd9f40c288dc2103cfe6cbe5a5bdbf23ba37804
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-25 01:03:12 -0400

    Adds basic board

--- challenge_b1/client/public/index.js
+++ challenge_b1/client/public/index.js
@@ -1,13 +1,45 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      boardRep: [[null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null]]
+    };
   }
 
   render() {
-    return React.createElement("div", null, "here is content");
+    return React.createElement(Board, {
+      boardRep: this.state.boardRep
+    });
   }
 
 }
+
+const Square = ({
+  x,
+  y,
+  piece
+}) => {
+  return React.createElement("div", {
+    class: "square-background"
+  }, `${x},${y}`, React.createElement("div", {
+    class: "square-content"
+  }, piece));
+};
+
+const Board = ({
+  boardRep
+}) => {
+  const flatArr = boardRep.reduce((acc, row, i) => {
+    return acc.concat(row.map((piece, j) => React.createElement(Square, {
+      key: `${i},${j}`,
+      y: i,
+      x: j,
+      piece: piece
+    })));
+  }, []);
+  return React.createElement("div", {
+    id: "board"
+  }, flatArr);
+};
 
 ReactDOM.render(React.createElement(App, null), document.querySelector('#root'));
--- challenge_b1/client/src/index.jsx
+++ challenge_b1/client/src/index.jsx
@@ -3,15 +3,45 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-
+      boardRep: [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null]
+      ]
     }
   }
 
   render() {
     return (
-      <div>here is content</div>
+      <Board boardRep={this.state.boardRep}></Board>
     );
   }
+}
+
+const Square = ({ x, y, piece}) => {
+  return (
+    <div class="square-background">
+      {`${x},${y}`}
+      <div class="square-content">{piece}</div>
+    </div>
+  );
+}
+
+const Board = ({ boardRep }) => {
+  const flatArr = boardRep.reduce((acc, row, i) => {
+    return acc.concat(row.map((piece, j) => <Square key={`${i},${j}`} y={i} x={j} piece={piece}></Square>));
+  }, []);
+
+  return (
+    <div id="board">
+      {flatArr}
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
