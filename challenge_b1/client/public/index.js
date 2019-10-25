6ba00c65496bbe02981424c2e59eaeb722ff49b6
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-25 15:02:17 -0400

    Adjusts colors

--- challenge_b1/client/public/index.js
+++ challenge_b1/client/public/index.js
@@ -12,7 +12,25 @@ class App extends React.Component {
       boardRep: [[null, 0, null, 0, null, 0, null, 0], [0, null, 0, null, 0, null, 0, null], [null, 0, null, 0, null, 0, null, 0], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [1, null, 1, null, 1, null, 1, null], [null, 1, null, 1, null, 1, null, 1], [1, null, 1, null, 1, null, 1, null]],
       boardRepTempate: [[null, 0, null, 0, null, 0, null, 0], [0, null, 0, null, 0, null, 0, null], [null, 0, null, 0, null, 0, null, 0], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [1, null, 1, null, 1, null, 1, null], [null, 1, null, 1, null, 1, null, 1], [1, null, 1, null, 1, null, 1, null]]
     };
-  }
+  } // WRONG APPROACH ??
+  // maintain an emptySquares object, with the x,y coordinates of every playable square that is empty
+  // on every turn, check each square in emptySquare
+  // if the immediate diagonal squares (above for 0's turn / below for 1's turn) have a piece
+  // this square is movable to from one / both diagonals
+  // __________
+  // else, the current player can't move to this square this turn
+  // maintain two piecesCanMove objects, one for each player, with the x,y coordinates of every piece for that player
+  // on every turn, check each piece in piecesCanMove
+  // for each immediate diagonal (above for 0's turn / below for 1's turn)
+  // if this diagonal is empty
+  // this square can be moved to
+  // update piecesCanMove object to reflect that
+  // else if this diagonal contains an enemy piece
+  // get the coordinates for the square this enemy piece is on
+  // repeat the diagonal checking process from this square // recursion ???
+  // else if this diagonal contains a friendly piece
+  // can't move here
+
 
   handleClick(e, x, y) {
     this.setState((state, props) => {
@@ -25,8 +43,6 @@ class App extends React.Component {
   }
 
   isGameOver() {}
-
-  ComponentDidUpdate() {}
 
   render() {
     return React.createElement(React.Fragment, null, React.createElement(Board, {
@@ -70,12 +86,15 @@ const Square = ({
   color,
   onClick
 }) => {
+  let pieceColor = piece === 0 ? 'black-piece' : 'red-piece';
   return React.createElement("div", {
     className: `square-background ${color}`,
     onClick: e => onClick(e, x, y)
-  }, React.createElement("div", {
+  }, `${x},${y}`, React.createElement("div", {
     className: "square-content"
-  }, piece));
+  }, React.createElement("span", {
+    className: pieceColor
+  }, piece)));
 };
 
 ReactDOM.render(React.createElement(App, null), document.querySelector('#root'));
--- challenge_b1/client/src/index.jsx
+++ challenge_b1/client/src/index.jsx
@@ -35,6 +35,31 @@ class App extends React.Component {
     }
   }
 
+  // WRONG APPROACH ??
+  // maintain an emptySquares object, with the x,y coordinates of every playable square that is empty
+    // on every turn, check each square in emptySquare
+      // if the immediate diagonal squares (above for 0's turn / below for 1's turn) have a piece
+        // this square is movable to from one / both diagonals
+          // __________
+      // else, the current player can't move to this square this turn
+
+
+
+
+
+  // maintain two piecesCanMove objects, one for each player, with the x,y coordinates of every piece for that player
+    // on every turn, check each piece in piecesCanMove
+      // for each immediate diagonal (above for 0's turn / below for 1's turn)
+        // if this diagonal is empty
+          // this square can be moved to
+          // update piecesCanMove object to reflect that
+        // else if this diagonal contains an enemy piece
+          // get the coordinates for the square this enemy piece is on
+          // repeat the diagonal checking process from this square // recursion ???
+        // else if this diagonal contains a friendly piece
+          // can't move here
+
+
   handleClick(e, x, y) {
     this.setState((state, props) => {
       return {
@@ -46,10 +71,6 @@ class App extends React.Component {
   }
 
   isGameOver() {
-
-  }
-
-  ComponentDidUpdate() {
 
   }
 
@@ -85,10 +106,12 @@ const Board = ({ boardRep, onClick }) => {
 }
 
 const Square = ({ x, y, piece, color, onClick }) => {
+  let pieceColor = piece === 0 ? 'black-piece': 'red-piece';
+
   return (
     <div className={`square-background ${color}`} onClick={(e) => onClick(e, x, y)}>
-      {/* {`${x},${y}`} */}
-      <div className="square-content">{piece}</div>
+      {`${x},${y}`}
+      <div className="square-content"><span className={pieceColor}>{piece}</span></div>
     </div>
   );
 }
