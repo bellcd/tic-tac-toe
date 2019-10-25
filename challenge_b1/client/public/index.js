49b6270e9fcfbe0323feb89bc50bc62665c98d77
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-25 16:17:03 -0400

    [wip] Determines moves

--- challenge_b1/client/public/index.js
+++ challenge_b1/client/public/index.js
@@ -3,40 +3,94 @@ class App extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      // 0s are white
-      // 1s are red
       turn: 1,
-      currentTurn: 0,
-      zeroScore: 0,
-      oneScore: 0,
-      boardRep: [[null, 0, null, 0, null, 0, null, 0], [0, null, 0, null, 0, null, 0, null], [null, 0, null, 0, null, 0, null, 0], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [1, null, 1, null, 1, null, 1, null], [null, 1, null, 1, null, 1, null, 1], [1, null, 1, null, 1, null, 1, null]],
-      boardRepTempate: [[null, 0, null, 0, null, 0, null, 0], [0, null, 0, null, 0, null, 0, null], [null, 0, null, 0, null, 0, null, 0], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], [1, null, 1, null, 1, null, 1, null], [null, 1, null, 1, null, 1, null, 1], [1, null, 1, null, 1, null, 1, null]]
+      currentTurn: 'black',
+      blackScore: 0,
+      redScore: 0,
+      boardRep: [[null, 'black', null, 'black', null, 'black', null, 'black'], ['black', null, 'black', null, 'black', null, 'black', null], [null, 'black', null, 'black', null, 'black', null, 'black'], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], ['red', null, 'red', null, 'red', null, 'red', null], [null, 'red', null, 'red', null, 'red', null, 'red'], ['red', null, 'red', null, 'red', null, 'red', null]],
+      boardRepTempate: [[null, 'black', null, 'black', null, 'black', null, 'black'], ['black', null, 'black', null, 'black', null, 'black', null], [null, 'black', null, 'black', null, 'black', null, 'black'], [null, null, null, null, null, null, null, null], [null, null, null, null, null, null, null, null], ['red', null, 'red', null, 'red', null, 'red', null], [null, 'red', null, 'red', null, 'red', null, 'red'], ['red', null, 'red', null, 'red', null, 'red', null]],
+      blackPiecesPossibleMoves: {
+        '1,0': {},
+        '3,0': {},
+        '5,0': {},
+        '7,0': {},
+        '0,1': {},
+        '2,1': {},
+        '4,1': {},
+        '6,1': {},
+        '1,2': {},
+        '3,2': {},
+        '5,2': {},
+        '7,2': {}
+      },
+      RedPiecesPossibleMoves: {
+        '0,5': {},
+        '2,5': {},
+        '4,5': {},
+        '6,5': {},
+        '1,6': {},
+        '3,6': {},
+        '5,6': {},
+        '7,6': {},
+        '0,7': {},
+        '2,7': {},
+        '4,7': {},
+        '6,7': {}
+      }
     };
-  } // WRONG APPROACH ??
-  // maintain an emptySquares object, with the x,y coordinates of every playable square that is empty
-  // on every turn, check each square in emptySquare
-  // if the immediate diagonal squares (above for 0's turn / below for 1's turn) have a piece
-  // this square is movable to from one / both diagonals
-  // __________
-  // else, the current player can't move to this square this turn
-  // maintain two piecesCanMove objects, one for each player, with the x,y coordinates of every piece for that player
-  // on every turn, check each piece in piecesCanMove
-  // for each immediate diagonal (above for 0's turn / below for 1's turn)
-  // if this diagonal is empty
-  // this square can be moved to
-  // update piecesCanMove object to reflect that
-  // else if this diagonal contains an enemy piece
-  // get the coordinates for the square this enemy piece is on
-  // repeat the diagonal checking process from this square // recursion ???
-  // else if this diagonal contains a friendly piece
-  // can't move here
+  } // maintain two ___PiecesPossibleMoves objects, one for each player, with the x,y coordinates of every piece for that player
+  // each list of possible moves for each piece starts out as an empty object
 
+
+  determinePossibleMoves(turn, x, y) {
+    const currentPlayer = turn;
+    const otherPlayer = turn === 'black' ? 'red' : 'black';
+    let list = turn === 'black' ? this.state.blackPiecesPossibleMoves : this.state.RedPiecesPossibleMoves; // going to directly assign to list later, THEN call setState to replace the relevant property in setState when list is ready. decent workflow??
+
+    let pieceX, pieceY, diagonalX, diagonalY; // on every turn, check each piece in ___PiecesPossibleMoves
+    // get an array of all the keys
+
+    const currentPiecePositions = Object.keys(list).map(coords => coords.split(',')); // for each piece
+
+    currentPiecePositions.forEach(piece => {
+      pieceX = piece[0];
+      pieceY = piece[1]; // for each immediate diagonal (above for 0's turn / below for 1's turn)
+
+      leftDiagonalX = pieceX - 1;
+      rightDiagonalX = pieceX + 1;
+
+      if (turn === 'black') {
+        leftDiagonalY = pieceY + 1;
+        rightDiagonalY = pieceY + 1;
+      } else if (turn === 'red') {
+        leftDiagonalY = pieceY - 1;
+        rightDiagonalY = pieceY - 1;
+      } // if this diagonal is empty
+
+
+      if (this.state.boardRep[leftDiagonalY][leftDiagonalX] === null) {
+        // this square can be moved to
+        // update ___PiecesPossibleMoves to reflect that
+        // make sure I can add these to the list object properly ?? react state??
+        if (list[`${x},${y}`].length === 0) {
+          list[`${x},${y}`] = [`${leftDiagonalX},${leftDiagonalY}`];
+        } else {
+          list[`${x},${y}`].push(`${leftDiagonalX},${leftDiagonalY}`);
+        } // else if this diagonal contains an enemy piece
+
+      } else if (this.state.boardRep[leftDiagonalY][leftDiagonalX] === otherPlayer) {// get the coordinates for the square this enemy piece is on
+        // repeat the diagonal checking process from this square // recursion ???
+      } else {// this diagonal contains a friendly piece, so can't move here
+        }
+    });
+  }
 
   handleClick(e, x, y) {
+    this.determinePossibleMoves(this.state.turn, x, y);
     this.setState((state, props) => {
       return {
         turn: ++state.turn,
-        currentTurn: state.currentTurn === 0 ? 1 : 0
+        currentTurn: state.currentTurn === 'black' ? 'red' : 'black'
       };
     });
     console.log(`you clicked on square ${x},${y}`);
@@ -86,7 +140,7 @@ const Square = ({
   color,
   onClick
 }) => {
-  let pieceColor = piece === 0 ? 'black-piece' : 'red-piece';
+  let pieceColor = piece === 'black' ? 'black-piece' : 'red-piece';
   return React.createElement("div", {
     className: `square-background ${color}`,
     onClick: e => onClick(e, x, y)
