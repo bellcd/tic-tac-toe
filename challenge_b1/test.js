15b602edb4cc4c02d9ac4f69f213052cf0065b8f
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-10-26 00:40:14 -0400

    Adds test suite

---
+++ challenge_b1/test.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const expect = require('chai').expect;
+
+describe('Board', () => {
+  describe('Possible Moves', () => {
+    describe('Black', () => {
+      it('Should calculate initial possible moves correctly', () => {
+        expect(true).to.be.true;
+      });
+    })
+  });
+});
